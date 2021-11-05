621fbd38101a3d901e0769805f4594c47915375b
Zoe Daswani <zdasw001@fiu.edu>
2021-11-05 01:21:07 -0400

    lab 1

---
+++ src/edu/fiu/sysdesign/Car.java
@@ -0,0 +1,26 @@
+package edu.fiu.sysdesign;
+
+public class Car {
+   
+    String color;
+    String model;
+    boolean isOn;
+
+
+    public void turnOn(){
+
+    }
+
+    public void turnOff(){
+        
+    } 
+
+}
+public static void main(String[] args) {
+    
+
+
+
+
+
+}
